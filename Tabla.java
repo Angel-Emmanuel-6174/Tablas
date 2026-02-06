9e1effc2ef802ae6a7e44562a83846f63ec05a47
Angel-Emmanuel-6174 <20253rd023@utez.edu.mx>
2026-02-06 12:28:23 -0600

    SE TERMINO EL PROGRAMA, MUESTRA LAS TABLAS DEL NUMERO HASTA EL 10

--- Tabla.java
+++ Tabla.java
@@ -3,6 +3,14 @@ import java.util.Scanner;
 public class Tabla {
 
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("BIENVENIDO\nTABLAS DE MULTIPLICAR");
+        int num;
+        System.out.println("QUE NUMERO QUIERES MOSTRAR?");
+        num = sc.nextInt();
+        for(int i=1; i<=10;i++){
+            System.out.println(num+" * "+i+" = "+(i*num));
+        }
 
 
 
@@ -10,6 +18,5 @@ public class Tabla {
 
 
 
-        
     }
 }
